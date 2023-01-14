6be36ac53c3966aa8d6dda68864ebb6317e531c7
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-01-14 18:59:58 -0300

    feature(grpc): create grpc server

---
+++ src/components/server/index.js
@@ -0,0 +1,70 @@
+import grpc from '@grpc/grpc-js'
+import protoLoader from '@grpc/proto-loader'
+import path from 'path'
+
+class Server {
+  constructor({logger}, config) {
+    this.config = config;
+    this.logger = logger.child({module: 'Server'})
+    const PROTO_PATH = `${path.resolve()}/components/grpc/protos/entities.proto`
+    const packageDefinition = protoLoader. loadSync(
+    PROTO_PATH,
+    {
+      keepCase: true,
+      longs: String,
+      enums: String,
+      defaults: true,
+      oneofs: true,
+    }
+    );
+    const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+    const { Entities } = protoDescriptor;
+    this.grpcServer = new grpc.Server();
+
+    this.grpcServer.addService(Entities.EntitiesService.service,{
+      createEntity: async (call, callback) => {
+        try {
+          callback(null, true)
+        } catch (err) {
+          callback({
+            code: grpc.status.INVALID_ARGUMENT,
+            message: err.message
+          })
+        }
+      },
+      updateEntity: async (call, callback) => {
+        try {
+          callback(null, true)
+        } catch (err) {
+          callback({
+            code: grpc.status.INVALID_ARGUMENT,
+            message: err.message
+          })
+        }
+      },
+      identifyEntities: async (call, callback) => {
+        try {
+          callback(null, {entities: ['tomi','dali']})
+        } catch (err) {
+          callback({
+            code: grpc.status.INVALID_ARGUMENT,
+            message: err.message
+          })
+        }
+      }
+    })
+  }
+  async run() {
+    await new Promise((resolve, reject) => {
+      this.grpcServer.bindAsync(`0.0.0.0:${this.config.port}`, grpc.ServerCredentials.createInsecure(), (err)=>{
+        if(err) {
+          reject(err)
+        }
+        resolve()
+      })
+    })
+    await this.grpcServer.start()
+  }
+}
+
+export default Server
