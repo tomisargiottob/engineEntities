e3810b89091aee9abc6a56c7081199ebd24283f9
Tomas Sargiotto <tomisargiottob@hotmail.com>
2023-01-21 12:47:49 -0300

    feature(entities): add database methods

--- src/components/database/collections/entities.js
+++ src/components/database/collections/entities.js
@@ -1,8 +1,27 @@
+import { v4 as uuid } from 'uuid'
+import Entity from '../models/entity'
+
 class Entities {
   constructor({db}) {
     this.db = db;
-    this.collection= this.db.collection('assistants')
+    this.collection= this.db.collection('entities')
   }
+
+  async create(entityData) {
+    const entity = await this.collection.insertOne({_id: uuid(), ...entityData});
+    return new Entity(entity)
+  }
+
+  async getAll(assistantId, skillsetId) {
+    const entities = await this.collection.findAll({assistantId, skillsetId});
+    return entities.map((entity) => new Entity(entity))
+  }
+
+  async findOne(assistantId, skillsetId, entityId) {
+    const entity = await this.collection.findOne({assistantId, skillsetId, _id:entityId});
+    return new Entity(entity)
+  }
+
 }
 
 export default Entities
--- src/components/database/models/entity.js
+++ src/components/database/models/entity.js
@@ -1,8 +1,28 @@
 class Entity {
   constructor(collection, data) {
-    this.id = data.id
+    this.id = data._id
     this.name = data.name
     this.collection = collection
+    this.synonyms = data.synonyms
+    this.regex = data.regex
+  }
+
+  async update(entityData) {
+    const updateData = {}
+    if(entityData.synonyms) {
+      updateData.synonyms = entityData.synonyms
+    }
+    if(entityData.name) {
+      updateData.name = entityData.name
+    }
+    if(entityData.regex) {
+      updateData.regex = entityData.regex
+    }
+    return this.collection.update(this.id, { $set: updateData})
+  }
+
+  async remove() {
+    return this.collection.remove(this.id)
   }
 }
 
